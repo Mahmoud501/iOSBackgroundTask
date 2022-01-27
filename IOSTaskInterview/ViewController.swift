c233d35164949aa434925c0f28f49737ed2194cf
MahmoudZaky2 <deep2012_1st@yahoo.com>
2022-01-27 14:02:59 +0200

    Initial Commit

---
+++ IOSTaskInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSTaskInterview
+//
+//  Created by Mahmoud Zaki on 27/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
